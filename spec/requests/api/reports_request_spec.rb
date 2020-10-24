5921358f2ceb40100150a6a64cce3f175e1d4485
dianakakoma <dikakoma@gmail.com>
2020-10-24 18:38:04 -0500

    created reports controller and corresponding test the create action.

--- spec/requests/api/reports_request_spec.rb
+++ spec/requests/api/reports_request_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Report, type: :model do
-  describe 'creation' do
-    it 'can be created' do
+RSpec.describe "Api::Reports", type: :request do
+  describe 'create' do
+    it "successfully creates a new report" do
       report = Report.create(name: "test name",description: "test description", suggested_fix: "test fix", url:"https://www.nytimes.com/", status: "test status", screenshot: "https://cdn.pixabay.com/photo/2015/05/11/13/28/software-762486_960_720.jpg")
 
-      expect(report).to be_valid
+      expect(Report.last.name).to equal("test name")
     end
   end
+
 end
