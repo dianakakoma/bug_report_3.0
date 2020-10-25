5d0ef902101cc9e7843a64a4a32a6a4de6f258d8
dianakakoma <dikakoma@gmail.com>
2020-10-24 21:42:54 -0500

    created the database and seeded it.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Report.create(name: "Toni Morrison",description: "There is an insect in my computer", suggested_fix: "Call Terminix", url:"https://www.nytimes.com/", status: "submitted", screenshot: "https://cdn.pixabay.com/photo/2015/05/11/13/28/software-762486_960_720.jpg")
+
+Report.create(name: "Maya Angelou", description: "The image is unrelated to the subject and therefore it is not useful.", suggested_fix: "Use or generate a helpful image", url:"https://www.nytimes.com/", status: "submitted", screenshot: "https://cdn.pixabay.com/photo/2015/05/11/13/28/software-762486_960_720.jpg")
+
+Report.create(name: "James Baldwin",  description: "Navigation bar should be color-coded to correspond with a specific client - site-wide", suggested_fix: "use primary colors", url:"https://www.nytimes.com/", status: "submitted", screenshot:"https://p0.pxfuel.com/preview/327/874/545/brain-filament-light-bulb.jpg")
+
+Report.create(name: "Toni Morrison", description: "The link to the user profile generates and error even though I am signed in.", suggested_fix: "This might a permissions issue.", url: "https://www.nytimes.com/", status: "submitted", screenshot: "https://cdn.pixabay.com/photo/2015/05/11/13/28/software-762486_960_720.jpg")
